64855c913e88bfdf93212e12a56ba944c2573bfd
1169794338@qq.com <Liziyi521521>
2020-05-07 03:20:11 +0800

    init

--- src/main/java/com/bootx/entity/AliyunSmsTemplate.java
+++
@@ -1,9 +0,0 @@
-package com.bootx.entity;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "bootx_aliyun_sms_Template")
-public class AliyunSmsTemplate extends BaseEntity<Long> {
-}
